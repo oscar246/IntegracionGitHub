13cfa905199d57ee87eb673808bc9f269be5950f
Oscar Giraldo <oscardavid@jmspro.net>
2016-04-24 00:34:24 -0500

    Agregacion de varible oscar y edgar

--- app/src/main/java/com/snw/integraciongithub/MainActivity.java
+++ app/src/main/java/com/snw/integraciongithub/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String oscar = "oscar";
+
+    private String edgar = "oscar";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
